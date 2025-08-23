41ac5ba22f5f49847ae47f011b5bda046016ded6
Silas Stephens <76085257+silasstephens15@users.noreply.github.com>
2025-08-22 21:55:21 -0400

    Added getHumanChoice() to game.js
    This will allow users to input their choice for the game.

--- game.js
+++ game.js
@@ -1,6 +1,14 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
     const choices = ['rock', 'paper', 'scissors'];
     let random = Math.random() * 2;
     random = Math.round(random);
     return choices[random]
+};
+
+function getHumanChoice(){
+    const choice = prompt('Enter rock, paper, or scissors: ');
+    return choice;
 };
