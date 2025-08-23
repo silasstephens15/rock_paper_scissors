745e4ec817783038e82d31e69a8691eccfc3f040
Silas Stephens <76085257+silasstephens15@users.noreply.github.com>
2025-08-22 22:07:58 -0400

    Added game logic to game.js
    The winner of the game can now be determined

--- game.js
+++ game.js
@@ -12,3 +12,45 @@ function getHumanChoice(){
     const choice = prompt('Enter rock, paper, or scissors: ');
     return choice;
 };
+
+function playRound(humanChoice, computerChoice){
+    if (humanChoice === 'rock'){
+        if (computerChoice === 'rock'){
+            console.log('Tie!')
+        };
+        if (computerChoice === 'paper'){
+            console.log('Computer Wins!');
+            computerScore++;
+        };
+        if (computerChoice === 'scissors'){
+            console.log('Human Wins!');
+            humanScore++;
+        };
+    };
+    if (humanChoice === 'paper'){
+        if (computerChoice === 'paper'){
+            console.log('Tie!')
+        };
+        if (computerChoice === 'scissors'){
+            console.log('Computer Wins!');
+            computerScore++;
+        };
+        if (computerChoice === 'rock'){
+            console.log('Human Wins!');
+            humanScore++;
+        };
+    };
+    if (humanChoice === 'scissors'){
+        if (computerChoice === 'scissors'){
+            console.log('Tie!')
+        };
+        if (computerChoice === 'rock'){
+            console.log('Computer Wins!');
+            computerScore++;
+        };
+        if (computerChoice === 'paper'){
+            console.log('Human Wins!');
+            humanScore++;
+        };
+    };
+};
