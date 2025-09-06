669c79f55e8ba80b6ae9f3da3940b468aab7c1d8
Silas Stephens <76085257+silasstephens15@users.noreply.github.com>
2025-09-06 02:31:59 -0400

    Added results div
    Added div to index.html and changed console.log() to display inside div

--- game.js
+++ game.js
@@ -1,69 +1,71 @@
-function getComputerChoice(){
-    const choices = ['rock', 'paper', 'scissors'];
-    let random = Math.random() * 2;
-    random = Math.round(random);
-    return choices[random]
-};
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  let random = Math.random() * 2;
+  random = Math.round(random);
+  return choices[random];
+}
 
-function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+  const results = document.querySelector("#results");
 
-    const buttons = document.querySelectorAll('button');
+  const buttons = document.querySelectorAll("button");
 
-    buttons.forEach(button => button.addEventListener('click', function()
-    {playRound(button.id, getComputerChoice())}));
+  buttons.forEach((button) =>
+    button.addEventListener("click", function () {
+      playRound(button.id, getComputerChoice());
+    })
+  );
 
-    function playRound(humanChoice, computerChoice){
-        if (humanChoice === 'rock'){
-            if (computerChoice === 'rock'){
-                console.log('Tie!')
-            };
-            if (computerChoice === 'paper'){
-                console.log('Computer won the round!');
-                computerScore++;
-            };
-            if (computerChoice === 'scissors'){
-                console.log('You won the round!');
-                humanScore++;
-            };
-        };
-        if (humanChoice === 'paper'){
-            if (computerChoice === 'paper'){
-                console.log('Tie!')
-            };
-            if (computerChoice === 'scissors'){
-                console.log('Computer won the round!!');
-                computerScore++;
-            };
-            if (computerChoice === 'rock'){
-                console.log('You won the round!');
-                humanScore++;
-            };
-        };
-        if (humanChoice === 'scissors'){
-            if (computerChoice === 'scissors'){
-                console.log('Tie!')
-            };
-            if (computerChoice === 'rock'){
-                console.log('Computer won the round!!');
-                computerScore++;
-            };
-            if (computerChoice === 'paper'){
-                console.log('You won the round!');
-                humanScore++;
-            };
-        };
-    };
-    if (humanScore > computerScore){
-        console.log('You Win!')
+  function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock") {
+      if (computerChoice === "rock") {
+        results.textContent = "Tie!";
+      }
+      if (computerChoice === "paper") {
+        results.textContent = "Computer won the round!";
+        computerScore++;
+      }
+      if (computerChoice === "scissors") {
+        results.textContent = "You won the round";
+        humanScore++;
+      }
     }
-    else if(humanScore < computerScore){
-        console.log('Computer Wins!')
+    if (humanChoice === "paper") {
+      if (computerChoice === "paper") {
+        results.textContent = "Tie!";
+      }
+      if (computerChoice === "scissors") {
+        results.textContent = "Computer won the round!";
+        computerScore++;
+      }
+      if (computerChoice === "rock") {
+        console.log("You won the round!");
+        humanScore++;
+      }
     }
-    else{
-        console.log('It is a Tie!')
+    if (humanChoice === "scissors") {
+      if (computerChoice === "scissors") {
+        results.textContent = "Tie!";
+      }
+      if (computerChoice === "rock") {
+        results.textContent = "Computer won the round!";
+        computerScore++;
+      }
+      if (computerChoice === "paper") {
+        console.log("You won the round!");
+        humanScore++;
+      }
     }
-};
+  }
+  if (humanScore > computerScore) {
+    results.textContent = "You win!";
+  } else if (humanScore < computerScore) {
+    console.log("Computer Wins!");
+  } else {
+    console.log("It is a Tie!");
+  }
+}
 
-playGame()
+playGame();
